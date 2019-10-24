7bdaa7fa319800afca93b65d4b8f865c44b0c7c3
szamon <szamon@wp.pl>
2019-10-24 17:51:58 +0200

    adjusted for gh-pages

--- config/webpack.common.config.js
+++ config/webpack.common.config.js
@@ -2,6 +2,8 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin"); 
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyWebPackPlugin = require("copy-webpack-plugin");
+const ghpages = require("gh-pages");
+
 module.exports = {
     entry: {
         main: "./src/index.js"
